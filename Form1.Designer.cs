a2fd4a4e94b14e8c1bb1e26b8b6e33632b44f170
rafaelsrvv <64434622+rafaelsrvv@users.noreply.github.com>
2020-04-27 18:52:56 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Novo_Emgucv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.LIGAR = new System.Windows.Forms.Button();
+            this.SalvarRosto = new System.Windows.Forms.Button();
+            this.Nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(96, 46);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(271, 185);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // LIGAR
+            // 
+            this.LIGAR.Location = new System.Drawing.Point(534, 69);
+            this.LIGAR.Name = "LIGAR";
+            this.LIGAR.Size = new System.Drawing.Size(127, 83);
+            this.LIGAR.TabIndex = 3;
+            this.LIGAR.Text = "DETECTAR E RECONHECER";
+            this.LIGAR.UseVisualStyleBackColor = true;
+            this.LIGAR.Click += new System.EventHandler(this.LIGAR_Click);
+            // 
+            // SalvarRosto
+            // 
+            this.SalvarRosto.Location = new System.Drawing.Point(534, 228);
+            this.SalvarRosto.Name = "SalvarRosto";
+            this.SalvarRosto.Size = new System.Drawing.Size(127, 80);
+            this.SalvarRosto.TabIndex = 4;
+            this.SalvarRosto.Text = "Salvar Rosto";
+            this.SalvarRosto.UseVisualStyleBackColor = true;
+            this.SalvarRosto.Click += new System.EventHandler(this.SalvarRosto_Click);
+            // 
+            // Nome
+            // 
+            this.Nome.Location = new System.Drawing.Point(534, 181);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(127, 20);
+            this.Nome.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.SalvarRosto);
+            this.Controls.Add(this.LIGAR);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button LIGAR;
+        private System.Windows.Forms.Button SalvarRosto;
+        private System.Windows.Forms.TextBox Nome;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Novo_Emgucv
+{
+    public partial class Form1 : Form
+    {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX,0.6d,0.6d);
+        HaarCascade facedetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayface = null;
+        List<Image<Gray, byte>> trainingimages = new List<Image<Gray, byte>>();
+        List<String> labels = new List<string>();
+        List<String> Users = new List<string>();
+        int Count, NumLables, t=0;
+        string name, names = null;
+
+        private void SalvarRosto_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            grayface = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] detectedfaces = grayface.DetectHaarCascade(facedetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach(MCvAvgComp f in detectedfaces[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray,Byte > ();
+                break;
+            }
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingimages.Add(TrainedFace);
+            labels.Add(Nome.Text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingimages.ToArray().Length.ToString() + ",");
+            for(int i = 1; i<trainingimages.ToArray().Length + 1; i++)
+            {
+                trainingimages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+            }
+            MessageBox.Show(Nome.Text + "Salvo com sucesso");
+        }
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //Haar cascade para detecção facial
+            facedetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath+"/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+                string FacesLoad;
+                for (int i=1; i<NumLables + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bpm";
+                    trainingimages.Add(new Image<Gray, byte>(Application.StartupPath + "Faces/Faces.txt"));
+
+                    labels.Add(Labels[1]);
+
+                }
+                //The first Label befora, will be the number of faces salved.
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("NADA NO BANCO DE DADOS");
+            }
+        }
+
+        private void LIGAR_Click(object sender, EventArgs e)
+        { 
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+
+
+
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayface = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayface.DetectHaarCascade(facedetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result=Frame.Copy(f.rect).Convert<Gray,Byte>().Resize(100,100,Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if(trainingimages.ToArray().Length !=0)
+                {
+                    MCvTermCriteria termCriteria = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingimages.ToArray(), labels.ToArray(), 1500, ref termCriteria);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+
+                }
+               // Users[t-1] = name;
+                Users.Add("");
+            }
+            imageBox1.Image = Frame;
+            names = "";
+            Users.Clear();
+        }
+    }
+}
